96e0078e38d719517f5b7d035a8134cae0fadc3a
Sixtine Caspar <sixcaspar@gmail.com>
2021-12-15 15:41:32 +0100

    added google analytics+ front

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# user
+# category
+# Category.create(name: 'Books')
+# Category.create(name: 'Movies')
+
+
+
 # movies
 
 require 'json'
